e2dabef27e3587bdf282250c3817efb549c5f58f
Daniel Parra Martin <danielparra.martin@ingenico.com>
2025-08-10 17:03:04 +0200

    First commit

---
+++ Emprendy/Emprendy/EmprendyApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmprendyApp.swift
+//  Emprendy
+//
+//  Created by Daniel Parra Martin on 10/8/25.
+//
+
+import SwiftUI
+
+@main
+struct EmprendyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
